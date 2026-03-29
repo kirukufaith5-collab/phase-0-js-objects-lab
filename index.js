f3be9b56ee134b51b7a05f67fccb50c3a4d37ba8
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-03-30 00:14:12 +0300

    add details of attendees

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 //Write your code here
+const attendee={
+attendeeId='T001',
+attendeeName ="Alice Smith",
+attendeeEvent="JavaScript Conference",
+ticketType="VIP",
+ticketPrice="150.00"}
 
+function logAttendeeName(attendee)
+console.log(attendee);
+
+function logTicketPrice(atendee)
+console.log(attendee);
+
+function updateTicketType(attendee,newticketType){
+  attendee.ticketPrice=newTicketPrice;
+}
+function removeEventProperty(attendee){
+delete attendee.attendeeEvent;}
+
+function addCheckedInProperty(attendee)_{
+attendee.checkedIn =true;
+}
 
 
 //Needed for the tests to work. Don't modify
